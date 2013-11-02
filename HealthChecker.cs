9f46200b180d522b91cc6c5348dc1364b76744e4
armand ndizigiye <ndiarmand11@yahoo.fr>
2013-11-02 19:04:39 +0100

    new changes

--- HealthChecker.cs
+++ HealthChecker.cs
@@ -4,14 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Net.Sockets;
 using System.Threading;
+using System.Diagnostics;
 
 namespace Proxy
 {
+    
+
     class HealthChecker
     {
-        public static string[] activeServer;
+        public event OnTextChangeHandler OnTextChangeEvent;
 
-
+        public static List<Server> availableServer;
 
         public static void checkHealth(string[] servers){
 
@@ -31,33 +34,63 @@ namespace Proxy
                 string[] serverAddressAndPort = {serverAddress,port};
 
                 var parameterizedThread = new ParameterizedThreadStart(PingServer);
-                var checkHealthThread = new Thread(parameterizedThread);
-                checkHealthThread.Start(serverAddressAndPort);
+                var pingThread = new Thread(parameterizedThread);
+                pingThread.Start(serverAddressAndPort);
             }
 
         }
 
         public static void PingServer(object o)
         {
-            var serverAddressAndPort = (string[]) o;
+            var serverAddressAndPort = (string[])o;
 
             var serverAddress = serverAddressAndPort[0];
             var serverPort = Convert.ToInt32(serverAddressAndPort[1]);
+            Server server = new Server(serverAddress,serverPort);
+            var isActive = server.Ping();
+        }
+
+    }
+
+        
+
+    class Server
+    {
+        public event OnTextChangeHandler OnTextChangeEvent;
+
+        private string address;
+        private int port;
+
+        public Server(string address, int port){
+            this.address = address;
+            this.port = port;
+        }
+
+        public bool Ping(){
 
             var destServerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            var testHeader = String.Format("GET http://{0}:{1}/ HTTP/1.1", serverAddress, serverPort);
+            var testHeader = String.Format("GET http://{0}:{1}/ HTTP/1.1", address, port);
             var testHeaderBytes = Encoding.ASCII.GetBytes(testHeader);
-            destServerSocket.Connect(serverAddress, serverPort);
+            destServerSocket.Connect(address, port);
             destServerSocket.Send(testHeaderBytes);
-            
-            var responseBuffer = 
 
-            while (destServerSocket.Receive(responseBuffer) != 0)
+            var responseBuffer = new byte[1024];
+
+            try
             {
-                //Console.Write(ASCIIEncoding.ASCII.GetString(responseBuffer));
-                _client.Send(responseBuffer);
-                Debug.Write(Encoding.ASCII.GetString(responseBuffer));
+                while (destServerSocket.Receive(responseBuffer) != 0)
+                {
+                    var response = Encoding.ASCII.GetString(responseBuffer);
+                    Debug.Write(Encoding.ASCII.GetString(responseBuffer));
+                }
+                destServerSocket.Disconnect(false);
+                destServerSocket.Dispose();
             }
+            catch (Exception e)
+            {
+                OnTextChangeEvent(new OnTextChange(e.Message));
+            }
+            return true;
         }
     }
 }
